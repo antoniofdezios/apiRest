411edeb9316b736f7f354fb1fa5c17eb2fe2436c
fernandezios <anthony_neutron-13-at@hotmail.com>
2019-09-24 21:32:04 +0200

    no message

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,7 +3,7 @@ from .models import Food
 from .serializers import FoodSerializer
 from rest_framework import viewsets
 
-class PostViewSet(viewsets.ModelViewSet):
+class FoodViewSet(viewsets.ModelViewSet):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
 # Create your views here.
