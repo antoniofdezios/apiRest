4f9eb71e178da3c8f8e5b3f68cd2ff6ae5952fac
fernandezios <anthony_neutron-13-at@hotmail.com>
2019-09-24 21:21:22 +0200

    no message

--- firstapp/models.py
+++ firstapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Food(models.Model):
-    foodId = models.IntegerField(default=1, primary_key=True, serialize=False, verbose_name='ID')),
+    foodId = models.IntegerField(default=1, primary_key=True, serialize=False, verbose_name='ID')
     name = models.CharField(default="Name",max_length = 200)
     brand = models.CharField(default="Brand",max_length = 200)
     
